97a07cdb339e110765cc84364d709745661ef811
Shifu <aemre3113@gmail.com>
2023-10-17 22:56:26 +0300

    List Oluşturuldu ve Sepete Ekle Butonuna Tıklayınca Listeye Ürün Ekleme İşleme Gerçekleştirildi

--- src/App.js
+++ src/App.js
@@ -1,34 +1,63 @@
 
-import { Container, SimpleGrid } from '@mantine/core';
+import { useState } from 'react'
+import { Container, SimpleGrid, List, ThemeIcon } from '@mantine/core';
 import './App.css';
 import CardComponent from './components/Card';
-
+import { IconCircleCheck, IconCircleDashed } from '@tabler/icons-react';
 
 const storeItems = [
   {
-    name: "A",
-    price: 20
+    name: "Televizyon",
+    price: 20000
   },
   {
-    name: "B",
-    price: 10
+    name: "Robot Süpürge",
+    price: 10000
   },
   {
-    name: "C",
-    price: 30
+    name: "Laptop",
+    price: 30000
   }
 ];
 
 function App() {
+  let [basketItems, setBasketItems] = useState([]);
   return (
-    <Container className="App">
-      <SimpleGrid cols={3}>
+    <Container>
+      <SimpleGrid cols={3} className='Store'>
         {
           storeItems.map(({ name, price }, index) => {
-            return <CardComponent name={name} price={price} key={index} onAdd={() => console.log("eklendi", name)} />
+            return (
+              <CardComponent
+                name={name}
+                price={price}
+                key={index}
+                onAdd={() => setBasketItems([...basketItems, { name }])}
+              />
+            )
           })
         }
       </SimpleGrid>
+      <List
+        className="List"
+        spacing="xs"
+        size="sm"
+        center
+        icon={
+          <ThemeIcon color="teal" size={24} radius="xl">
+            <IconCircleCheck size="1rem" />
+          </ThemeIcon>
+        }
+      >
+        {
+          basketItems.map(({ name }, index) => {
+            return (
+              <List.Item key={index}>{name}</List.Item>
+            )
+          })
+        }
+
+      </List>
     </Container>
   );
 }
